a19ac9c621d4a122cd207a251d39200d580af4c6
jach0025 <jchen02@bvsd.org>
2024-09-12 20:53:10 +0000

    arraystuff

--- IntegerArrayList.java
+++ IntegerArrayList.java
@@ -1,2 +1,17 @@
 public class IntegerArrayList extends IntegerList{
+    private int[] data;
+    private int size;
+
+    public IntegerArrayList() {
+        data = new int[10];
+        size = 0;
+    }
+
+    public void add(Integer val) {
+
+    }
+
+    public void add(int index, Integer val) {
+        
+    }
 }
